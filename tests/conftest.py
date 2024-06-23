af0e1565aae20b39849bafc08e55b4b8ea21948f
Andy Caine <me@andycaine.com>
2024-06-23 17:27:04 +0100

    Correct param in template.yaml and set fake aws creds

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+@pytest.fixture(autouse=True)
+def aws_creds(monkeypatch):
+    # Make sure that no tests try to use real AWS creds
+    monkeypatch.setenv('AWS_ACCESS_KEY_ID', 'testing')
+    monkeypatch.setenv('AWS_SECRET_ACCESS_KEY', 'testing')
+    monkeypatch.setenv('AWS_SECURITY_TOKEN', 'testing')
+    monkeypatch.setenv('AWS_SESSION_TOKEN', 'testing')
+    monkeypatch.setenv('AWS_DEFAULT_REGION', 'us-east-1')
